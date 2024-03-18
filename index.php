0c329b66da5290b69cc57e8d130499baae98f494
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-03-18 15:21:58 +0100

    upload

--- index.php
+++ index.php
@@ -7,6 +7,28 @@ $sqlargs = array("localhost","root","","pagehost");
 $conRet = validateDBConnection( connectDB($sqlargs) );
 if (is_string($conRet)) { echo $conRet;$dbInstance=null; } else { $dbInstance = $conRet; }
 
-$res = addPostAccessee($dbInstance, "accessees", 1, 2);
-print_r($res);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PageHost</title>
+    <link href="./images/favicon.png" type="image/png">
+</head>
+<body>
+    <header>
+        <div id="nav-bar" class="flex-horiz">
+            <div id="nav-titling" class="flex-vertic">
+                <div id="nav-logo-wrapper">
+                    <img src="./images/favicon.png" alt="Logo">
+                </div>
+                <div id="nav-pagename">
+                    <h1>PageHost</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
